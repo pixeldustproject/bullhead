9d5687775c8d2ec97d4c1448655152c0a59a5389
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-11-12 17:08:56 +0530

    timer: Queue timers on least power cpu
    There is potential power benefit by offloading timer activity to cpus
    of lesser power cost (power cluster). Both high-res and low-res timers
    that are not pinned to one cpu are now enqueued on first online CPU
    found in least shallow C-state in power-cluster.
    
    CRs-Fixed: 764251
    Change-Id: I2cea26c76972b566dfbfed084e377811a8784172
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    # Conflicts:
    #	kernel/sched/sched.h

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -772,6 +772,8 @@ extern void sched_account_irqtime(int cpu, struct task_struct *curr,
 unsigned int cpu_temp(int cpu);
 extern unsigned int nr_eligible_big_tasks(int cpu);
 
+extern int power_delta_exceeded(unsigned int cpu_cost, unsigned int base_cost);
+
 /*
  * 'load' is in reference to "best cpu" at its best frequency.
  * Scale that in reference to a given cpu, accounting for how bad it is
