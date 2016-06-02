bc8dd48e24112b8fb82155fd7cc6d43a1ed6b707
Jaegeuk Kim <jaegeuk@kernel.org>
2016-06-02 16:45:12 -0700

    f2fs: Sync to Upstream (4.7-rc1-3.10)
    * Up to date as of commit:
    f2fs: adjust other changes: http://git.kernel.org/cgit/linux/kernel/git/jaegeuk/f2fs-stable.git/commit/?h=linux-3.10.y&id=3fb1acec3f18c96b53fd2b9a0e1c610abd150565
    
    Conflicts:
    	fs/f2fs/super.c
    
    Change-Id: I186667944c057eeb58d5bf8c8bce54b11f201447
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    
    # Conflicts:
    #	fs/crypto/keyinfo.c
    #	fs/f2fs/Kconfig
    #	fs/f2fs/acl.c
    #	fs/f2fs/checkpoint.c
    #	fs/f2fs/data.c
    #	fs/f2fs/debug.c
    #	fs/f2fs/dir.c
    #	fs/f2fs/extent_cache.c
    #	fs/f2fs/f2fs.h
    #	fs/f2fs/file.c
    #	fs/f2fs/gc.c
    #	fs/f2fs/inline.c
    #	fs/f2fs/inode.c
    #	fs/f2fs/node.c
    #	fs/f2fs/recovery.c
    #	fs/f2fs/segment.c
    #	fs/f2fs/segment.h
    #	fs/f2fs/super.c
    #	fs/f2fs/xattr.c
    #	include/linux/fscrypto.h
    #	include/trace/events/f2fs.h

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -45,6 +45,8 @@ struct vfsmount;
 struct cred;
 struct swap_info_struct;
 struct seq_file;
+struct fscrypt_info;
+struct fscrypt_operations;
 struct workqueue_struct;
 struct fscrypt_info;
 struct fscrypt_operations;
